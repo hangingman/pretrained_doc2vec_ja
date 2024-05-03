1cef634c7f8e0ec5cb753a600eeec70bfeb26a29
hiroyuki.nagata <idiotpanzer@gmail.com>
2024-05-03 23:30:25 +0900

    Update parse_cirrus procedure

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pretrained doc2vec models on Japanese Wikipedia
+
 This is a repository of doc2vec models trained on Japanese Wikipedia corpus.
 
 Please See [my blog](https://yag-ays.github.io/project/pretrained_doc2vec_wikipedia/) for more details.
@@ -13,7 +14,25 @@ Please See [my blog](https://yag-ays.github.io/project/pretrained_doc2vec_wikipe
   - Compressed file size: 8.86 GB
 
 
-## Training setting
+## Training
+
+### Procedure
+
+```shell
+$ asdf local python 3.10.13
+$ python -m venv .venv --prompt doc2vec_ja
+$ source .venv/bin/activate
+$ python -m pip install --upgrade pip
+$ pip install -r requirements.txt
+```
+
+
+```shell
+$ python ./src/parse_cirrus.py -i /path/to/jawiki-20240422-cirrussearch-content.json.gz -o /path/to/20240422-cirrus_all.tsv.bz2
+```
+
+
+
 ### Parameters
 
 | param         | dbow300d | dmpv300d |
@@ -29,13 +48,14 @@ Please See [my blog](https://yag-ays.github.io/project/pretrained_doc2vec_wikipe
 
 ### Corpus
 
-- [`jawiki-20190114-cirrussearch-content.json.gz`](https://dumps.wikimedia.org/other/cirrussearch/20190114/)
+- [`jawiki-20240422-cirrussearch-content.json.gz`](https://dumps.wikimedia.org/other/cirrussearch/20240422/)
 
 ### Enviornments
 
 - Tokenizer
-  - MeCab: 0.996
+  - mecab-python3: 1.0.9
   - NEologd: Periodic data update on 2019-01-17(Thu)
+
 - requirements
-  - gensim:3.7.0
-  - numpy:1.16.0
+  - gensim: 3.7.0
+  - numpy: 1.26.4
--- src/parse_cirrus.py
+++ src/parse_cirrus.py
@@ -1,23 +1,40 @@
+import argparse
 import bz2
 import gzip
 import json
-import pickle
 
 import MeCab
 from tqdm import tqdm
 
 
-wakati = MeCab.Tagger("-O wakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-wakati.parse("")
+def count_lines_in_gzip_file(file_path):
+    with gzip.open(file_path, 'rt') as f:
+        line_count = sum(1 for line in f)
+    return line_count
 
-cirrus_all = {}
-with gzip.open("data/jawiki-20190114-cirrussearch-content.json.gz") as fin:
-    with bz2.open("data/20190114cirrus_all.tsv.bz2", 'wt') as fout:
-        for line in tqdm(fin, total=2271620):
-            json_line = json.loads(line)
-            if "index" not in json_line:
-                title = json_line["title"]
-                text = json_line["text"]
 
-                if title and text:
-                    print("\t".join([title, wakati.parse(text).strip()]), file=fout)
+def parse(args):
+    wakati = MeCab.Tagger("-O wakati")
+    wakati.parse("")
+
+    total_lines = count_lines_in_gzip_file(args.input)
+    with gzip.open(args.input) as fin:
+        with bz2.open(args.output, 'wt') as fout:
+            for line in tqdm(fin, total=total_lines):
+                json_line = json.loads(line)
+                if "index" not in json_line:
+                    title = json_line["title"]
+                    text = json_line["text"]
+
+                    if title and text:
+                        print("\t".join([title, wakati.parse(text).strip()]), file=fout)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="wikipediaのデータセットをパースしてTSV化する")
+    parser.add_argument("-i", "--input", type=str, required=True,
+                        help="wikipediaのJSONファイルのパス e.g. 'data/jawiki-20190114-cirrussearch-content.json.gz'")
+    parser.add_argument("-o", "--output", type=str, required=True,
+                        help="タイトルとテキストをTSV化したファイルの出力先パス e.g. 'data/20190114cirrus_all.tsv.bz2'")
+    args = parser.parse_args()
+    parse(args)
